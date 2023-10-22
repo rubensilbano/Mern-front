3a26f1b16245a5574b6f56b439c21339f3083501
Ruben Quintanilla <rubensilbano1990@gmail.com>
2023-10-22 16:21:56 -0300

    BOOTSTRAP INSTALADO

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
 import {ToastContainer} from "react-toastify"
 import './App.css';
 // import logo from './logo.svg';
